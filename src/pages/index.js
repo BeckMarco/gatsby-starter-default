99a54672a20ce3dcea3515f0faff92f8d4b24ea1
Marco Beck <68370603+BeckMarco@users.noreply.github.com>
2022-02-25 22:38:04 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,33 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
+                        <div class="content">
+                <span id="S.177r" class="pb"><br/><a class="external" href="http://resolver.staatsbibliothek-berlin.de/SBB0000DAA300000002">[177r]</a><br/></span>
+                <div class="opener">
+                    <h5 class="salute">Lieber <a class="person" href="../../../register/personen/P.0000001.html">Walden</a>,</h5>
+                </div>
+                <p>ich habe nicht das geringste vom Anwalt erhalten; Sie kennen mich ja, daß ich
+                    stets sofort antworte; es muß verloren gegangen sein. Grüßen Sie bitte D<sup><span class="underline">r</span></sup>
+                    <a class="person" href="../../../register/personen/P.0000056.html">Feige</a> und sagen Sie ihm
+                    das.</p>
+                <p>In einer niederrheinischen Zeitung soll von einer bevorstehenden großen
+                    Ausstellung in <a class="locality" href="../../../register/orte/O.0000045.html">Barmen</a> von <a class="person" href="../../../register/personen/P.0000009.html">Kandinsky</a> mir u.s.w.
+                    berichtet worden sein; ich kann mir nur denken, daß die <a class="person" href="../../../register/personen/P.0000057.html">Reiche</a>-<a class="locality" href="../../../register/orte/O.0000032.html">Dresden</a> Collection dahin
+                        kommt;<span id="S.177v.02" class="pb"><br/><a class="external" href="http://resolver.staatsbibliothek-berlin.de/SBB0000DAA300000001">[177v]</a><br/></span> ich
+                    bin natürlich einverstanden, nur soll die frei<span class="underline">e
+                    Rück</span>fracht <a class="locality" href="../../../register/orte/O.0000045.html">Barmen</a> – <a class="locality" href="../../../register/orte/O.0000002.html">Berlin</a> gesichert
+                        sein.<sup class="fn"><a href="#ap-1" id="fn-1">1</a></sup> Aber es ist natürlich nur Vermutung von mir. Von
+                        <a class="person" href="../../../register/personen/P.0000057.html">Reiche</a> höre ich gar nichts.</p>
+                <div class="closer">
+                                <p>
+                    <salute>Hrzl.</salute>
+                    <span class="signed">Ihr <a class="person" href="../../../register/personen/P.0000003.html">F.
+                            Marc</a>
+                    </span>
+                </p>
+                            </div>
+            </div>
+
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
